1cfe0936effeb2b16bcc5baa51080a3d77128f80
Vinicius Alves Martins <vinicius.martins1196@gmail.com>
2022-08-17 22:29:14 -0300

    teste mfe react

---
+++ apps/web/remote/src/app/App.tsx
@@ -0,0 +1,5 @@
+import RemoteEntry from './components/RemoteEntry'
+
+export default function App() {
+  return <RemoteEntry></RemoteEntry>
+}
---
+++ apps/web/remote/src/app/components/RemoteEntry.tsx
@@ -0,0 +1,7 @@
+export default function RemoteEntry() {
+    return (
+        <div>
+            <h3>Remote Entry</h3>
+        </div>
+    )
+}
